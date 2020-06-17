a23437dd6fc7be58cdf85049608af3b5eb35deba
Zoe Keller <keller.zoe98@gmail.com>
2020-06-17 12:09:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
-
+class Student < User 
+  
+  
+  def initialize
+    @knowledge = [] 
+  end 
+  
+  def learn(information)
+    @knowledge << information 
+  end 
+  
+  def knowledge 
+    @knowledge 
+  end 
+  
 end
